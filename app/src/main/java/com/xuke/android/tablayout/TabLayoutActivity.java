b6bbc8358b35345af92839d7afbc4e56e142bca1
xuke <228619770@qq.com>
2018-11-14 17:36:12 +0800

    tablayout

--- app/src/main/java/com/xuke/android/tablayout/TabLayoutActivity.java
+++ app/src/main/java/com/xuke/android/tablayout/TabLayoutActivity.java
@@ -1,14 +1,14 @@
 package com.xuke.android.tablayout;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 
 /**
  * Created by kekex on 2018/11/14.
  */
 
-public class TabLayoutActivity  extends Activity{
+public class TabLayoutActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
