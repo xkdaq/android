cbc527db257d2733a333b66f1d6ecdb316b657df
xuke <228619770@qq.com>
2018-11-14 16:23:30 +0800

    1111

---
+++ app/src/main/java/com/xuke/android/tablayout/TabLayoutActivity.java
@@ -0,0 +1,18 @@
+package com.xuke.android.tablayout;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by kekex on 2018/11/14.
+ */
+
+public class TabLayoutActivity  extends Activity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
